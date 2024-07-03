1fc4dcac04e3badee2f017b6c1fc5a9cea9b59b8
bon4567 <bon4567@gmail.com>
2024-07-03 17:09:08 +0900

    기초공사

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { dummy } from "./data/movieDummy.js";
+import Movie from "./components/Movie.jsx";
+import MovieDetail from "./components/MovieDetail.jsx"; // 상세 페이지 컴포넌트
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              dummy.results.map((item) => (
+                <Movie
+                  key={item.id}
+                  id={item.id} // id 전달
+                  title={item.title}
+                  poster_path={item.poster_path}
+                  vote_average={item.vote_average}
+                />
+              ))
+            }
+          />
+          <Route path="/detail/:id" element={<MovieDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
